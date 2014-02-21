fce37cc700e7f8172b78e227838278898db30640
Molham <molham225@gmail.com>
2014-02-21 21:36:21 +0200

    login page translated to arabic..

--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,7 @@
 <?php
 	//by defalut there is no error and only greeting message
 	$class="alert alert-info";
-	$message = "How are you today?";
+	$message = "كيف حالك اليوم؟";
 	
 	//error message
 	
@@ -20,7 +20,7 @@
 				
 		<!--<form action="<?php echo base_url();?>adminpanel/login/validateLogin" method="post" class="form-signin">-->
 		<form action="<?php echo base_url();?>login/validateLogin" method="post" class="form-signin">	
-			<h2 class="form-signin-heading">Please sign in</h2>
+			<h2 class="form-signin-heading">تسجيل الدخول</h2>
 			
 			<!-- error message -->
 	
@@ -30,18 +30,13 @@
 			</div>
 			
 			<!-- username -->
-			<input name="username" type="text" class="input-block-level" placeholder="Username" autofocus="true" required value="" /> 
+			<input name="username" type="text" class="input-block-level" placeholder="اسم المستخدم" autofocus="true" required value="" /> 
 			
 			<!-- passowrd -->
-			<input name="password" type="password" class="input-block-level" placeholder="Password" required>
-			
-			<!-- remmember me -->
-			<label class="checkbox">
-			  <input type="checkbox" value="remember-me" id="rememberme"> Remember me
-			</label>
+			<input name="password" type="password" class="input-block-level" placeholder="كلمة السر" required>
 			
 			
-			<button class="btn btn-large btn-primary" type="submit">Sign in</button>
+			<button class="btn btn-large btn-primary" type="submit">الدخول</button>
 							
 		</form>
 
